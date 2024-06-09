038b57f44e1a6918f7e716ca6ab2b2dafb88df6e
Vitaliy <120054505+VitaliyOne@users.noreply.github.com>
2024-06-09 13:59:00 +0300

    Add main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { City } from './types';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from "./footer/footer.component";
 import { HeaderComponent } from "./header/header.component";
+import { MainComponent } from "./main/main.component";
 
 @Component({
   selector: 'app-root',
@@ -14,30 +14,10 @@ import { HeaderComponent } from "./header/header.component";
     [RouterOutlet],
     CommonModule,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent
   ]
 })
 export class AppComponent {
   title = 'list-cities';
-  isListViewActive: boolean = true;
-  isTileViewActive: boolean = false;
-  isListView: boolean = true;
-  cities: City[] = [
-    { id: 1, image: 'https://screenshots.enkod.tech/ilya_novikovG6KIVEp3v160xCMY.png', name: 'Москва', description: 'Столица России, город федерального значения', favorite: false },
-    { id: 2, image: 'https://screenshots.enkod.tech/ilya_novikovW7s6gLwAGDjaJbNL.png', name: 'Санкт-Петербург', description: 'Второй по численности населения город России', favorite: false },
-    { id: 3, image: 'https://screenshots.enkod.tech/ilya_novikovHyQpb3vrjFvTfzJJ.png', name: 'Новосибирск', description: 'Третий по численности населения город России', favorite: false },
-    { id: 4, image: 'https://screenshots.enkod.tech/ilya_novikovvUvY8ocQ8yIQg6Gi.png', name: 'Екатеринбург', description: 'Город-миллионник в России. Тоже красивый город', favorite: false },
-    { id: 5, image: 'https://screenshots.enkod.tech/ilya_novikovHWEuBOtzexaZ3VQs.png', name: 'Нижний Новгород', description: 'Город в центральной России. Красивый город.', favorite: false },
-    { id: 6, image: 'https://screenshots.enkod.tech/ilya_novikovvjgSDr7xZl6A2UmH.png', name: 'Челябинск', description: 'Город в Российской Федерации, седьмой по количеству жителей', favorite: false },
-    { id: 7, image: 'https://screenshots.enkod.tech/ilya_novikovc2dbr7KvIJ2dknlZ.png', name: 'Казань', description: 'Город в России, столица Республики Татарстан.', favorite: false }
-  ];
-  toggleView(view: string) {
-    if (view === 'list') {
-      this.isListViewActive = true;
-      this.isTileViewActive = false;
-    } else if (view === 'tile') {
-      this.isListViewActive = false;
-      this.isTileViewActive = true;
-    }
-  }
 }
