7f84cb47080f2090f2b3eb834daea599153875ce
Vitaliy <120054505+VitaliyOne@users.noreply.github.com>
2024-06-09 22:17:12 +0300

    Add city creation form

--- src/app/create-city/create-city.component.ts
+++ src/app/create-city/create-city.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-create-city',
   standalone: true,
   templateUrl: './create-city.component.html',
   styleUrl: './create-city.component.css',
-  imports: [HeaderComponent]
+  imports: [HeaderComponent, RouterLink]
 })
 export class CreateCityComponent {
   headerTitle: string = 'Создание города';
